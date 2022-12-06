d642d71954a37237bffc2af81e8ad8e36b8033f7
daver987 <daveyalexander@outlook.com>
2022-12-05 22:46:06 -0500

    Add: Basic Cancel Page

--- pages/Cancel.vue
+++ pages/Cancel.vue
@@ -1,14 +1,11 @@
 <script lang="ts" setup></script>
 
 <template>
-  <section class="p-8">
-    <p>
+  <section class="min-h-screen bg-primary">
+    <Navigation />
+    <h2>
       Forgot to add something to your cart? Shop around then come back to pay!
-    </p>
-
-    <NuxtLink to="checkout?name=mevid" class="px-10 py-2 bg-green-700">
-      Navigate to Checkout
-    </NuxtLink>
+    </h2>
   </section>
 </template>
 
